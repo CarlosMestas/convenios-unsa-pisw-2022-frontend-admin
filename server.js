29996dc18bf7b4842a2ec7783bc15f0a7c6016d6
KELVIN TICLLAHUANACO <kticllahuanaco@unsa.edu.pe>
2022-10-19 00:04:44 -0500

    fix express problem3 deploy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/convenios-unsa-pisw-2022-admin'));
+app.use(express.static('./dist/convenios-unsa-pisw-2022-frontend-admin'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/convenios-unsa-pisw-2022-admin/'}),
+    res.sendFile('index.html', {root: 'dist/convenios-unsa-pisw-2022-frontend-admin/'}),
 );
 
 // Start the app by listening on the default Heroku port
